7297c4e0cd969f61b9ef1c9b911ae1c96bf3a903
Pawar <pawar1550@gmail.com>
2024-09-17 09:29:11 -0400

    Add files via upload

---
+++ Transient-Ischemic-Attack.R
@@ -0,0 +1,3 @@
+# Transient Ischemic Attack (TIA) prediction using support vector machines (SVM), Collaboration with Dr. A. Hande, Department of Neurological Surgery, Fortis Hospital, Navi Mumbai. https://www.drashokhande.com/about-us
+# Variables examples weight, height, BMI, physical activity, diet, smoking, etc.
+
